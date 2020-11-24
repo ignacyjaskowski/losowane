1d1811078a96e822fbe3ea26cf88887bd1cb6049
ignacyjaskowski <74858286+ignacyjaskowski@users.noreply.github.com>
2020-11-24 12:04:44 +0100

    wjente

--- main.ts
+++ main.ts
@@ -1,17 +1,10 @@
 function posortuj () {
-    for (let index = 0; index <= list.length; index++) {
-        b = list[b]
-        list[b - 1] = list[b - 1]
-    }
+	
 }
 function wylosuj_liste (dlugosc_listy: number) {
-    for (let index = 0; index < dlugosc_listy; index++) {
-        list.push(randint(0, 10))
-    }
+	
 }
-let b = 0
 let list: number[] = []
-list = []
 basic.forever(function () {
 	
 })
