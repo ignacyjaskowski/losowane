9b6dc0444dfa7e71262acaf2acb61c3d253e5e71
ignacyjaskowski <74858286+ignacyjaskowski@users.noreply.github.com>
2020-11-24 10:50:16 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,13 @@
+function posortuj () {
+	
+}
+function wylosuj_liste (dlugosc_listy: number) {
+    for (let index = 0; index < dlugosc_listy; index++) {
+        list.push(randint(0, 10))
+    }
+}
+let list: number[] = []
+list = []
+basic.forever(function () {
+	
+})
