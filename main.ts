cae35765beb21fbbef62b8e3931a69a0f97eb3e9
ignacyjaskowski <74858286+ignacyjaskowski@users.noreply.github.com>
2020-11-24 11:22:04 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,11 +1,15 @@
 function posortuj () {
-	
+    for (let index = 0; index <= list.length; index++) {
+        b = list[b]
+        list[b - 1] = list[b - 1]
+    }
 }
 function wylosuj_liste (dlugosc_listy: number) {
     for (let index = 0; index < dlugosc_listy; index++) {
         list.push(randint(0, 10))
     }
 }
+let b = 0
 let list: number[] = []
 list = []
 basic.forever(function () {
